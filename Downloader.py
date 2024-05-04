bade6959abb473854ade709195cf5a2dee3c57de
Timboman <36910038+Timboman@users.noreply.github.com>
2024-05-04 16:41:51 -0400

    Update Script
    Converted to Python 3.X
    Fixed URL Filtering issues

--- Downloader.py
+++ Downloader.py
@@ -5,37 +5,37 @@
 
 import os
 import errno
-from urllib2 import urlopen
-from urllib import urlretrieve
+from urllib.request import urlopen
+from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 
-print "                                    _           _ _     __     __ "
-print "  ____                             | |         | | |   |  \\   /  |"
-print " |  _ \ _ __ __ _  __ _  ___  _ __ | |__   __ _| | |   |   \\ /   |"
-print " | | | | '__/ _` |/ _` |/ _ \\| '_ \\| '_ \\ / _` | | |   |  |` `|  |"
-print " | |_| | | | (_| | (_| | (_) | | | | |_) | (_| | | |   |  |   |  |"
-print " |____/|_|  \\__,_|\\__, |\\___/|_| |_|_.__/ \\__,_|_|_|   |__|   |__|"
-print "                   __/ |"                            
-print "                  |___/"                       
-print "     ____                      _                 _"
-print "    |  _ \  __ __      _ _ __ | | ___   __ _  __| | ___  _ __ "
-print "    | | | |/ _ \\ \\ /\\ / | '_ \\| |/ _ \\ / _` |/ _` |/ _ \\| '__| " 
-print "    | |_| | (_)|\\ V  V /| | | | | (_) | (_| | (_| |  __/| |   "
-print "    |____/ \\___/ \\_/\\_/ |_| |_|_|\\___/ \\__,_|\\__,_|\\___||_| "
-print "                                                            -by dspec13\n"
+print("                                    _           _ _     __     __ ")
+print("  ____                             | |         | | |   |  \\   /  |")
+print(" |  _ \ _ __ __ _  __ _  ___  _ __ | |__   __ _| | |   |   \\ /   |")
+print(" | | | | '__/ _` |/ _` |/ _ \\| '_ \\| '_ \\ / _` | | |   |  |` `|  |")
+print(" | |_| | | | (_| | (_| | (_) | | | | |_) | (_| | | |   |  |   |  |")
+print(" |____/|_|  \\__,_|\\__, |\\___/|_| |_|_.__/ \\__,_|_|_|   |__|   |__|")
+print("                   __/ |")                            
+print("                  |___/")                       
+print("     ____                      _                 _")
+print("    |  _ \  __ __      _ _ __ | | ___   __ _  __| | ___  _ __ ")
+print("    | | | |/ _ \\ \\ /\\ / | '_ \\| |/ _ \\ / _` |/ _` |/ _ \\| '__| ") 
+print("    | |_| | (_)|\\ V  V /| | | | | (_) | (_| | (_| |  __/| |   ")
+print("    |____/ \\___/ \\_/\\_/ |_| |_|_|\\___/ \\__,_|\\__,_|\\___||_| ")
+print("                                                            -by dspec13\n")
 
 
 # Create Directory to be imported into Kindle Comic Converter
 try:
         os.mkdir("Dragon Ball Multiverse")
-        print "Directory \"Dragon Ball Multiverse\" was created successfully!"
+        print("Directory \"Dragon Ball Multiverse\" was created successfully!")
 except OSError as e:
         if e.errno == errno.EEXIST:
-                print "Directory \"Dragon Ball Multiverse\" already exists."
+                print("Directory \"Dragon Ball Multiverse\" already exists.")
         else:
                 raise
 
-print "\nBeginning Downloads:"
+print("\nBeginning Downloads:")
 
 # For Every Page of the Manga
 currentNumberOfPagesPublished = 1714
@@ -66,7 +66,8 @@ for x in range(0, currentNumberOfPagesPublished + 1):
                 unfilteredURL = lineContainingURL[startingIndex:endingIndex]
 
                 # Remove all occurences of amp; from url
-                filteredURL = unfilteredURL.replace("amp;", "")        
+                filteredURLA = unfilteredURL.replace("amp;", "")        
+                filteredURL = filteredURLA.replace(" ","")        
 
                 # Finally Store Real Image URL for Download
                 imageURL = "https://www.dragonball-multiverse.com" + filteredURL
@@ -75,7 +76,7 @@ for x in range(0, currentNumberOfPagesPublished + 1):
                 newFileName = "Dragon Ball Multiverse/%d.png" % x
                 urlretrieve(imageURL, filename=newFileName)
                 
-                print "Page %d Downloaded" % x
+                print("Page %d Downloaded" % x)
         elif x == 1000:
                 urlretrieve("https://www.dragonball-multiverse.com/imgs/pages_1000/AlbertoCubatas.jpg", filename="Dragon Ball Multiverse/1000.png")
         elif x == 1190:
@@ -87,40 +88,40 @@ for x in range(0, currentNumberOfPagesPublished + 1):
 
 
 
-print ""
-print "             ___     -._"
-print "             `-. \"\"\"--._ `-."
-print "                `.      \"-. `."
-print " _Seal_           `.       `. \\"
-print " `-.   \"\"\"---.._    \\        `."
-print "    `-.         \"-.  \\         `\\"
-print "       `.          `-.\\          \\_.-\"\"\"\"\"\"\"\"--._"
-print "         `.           `                          \"-."
-print "           `.                                       `.    __....-------..."
-print " --..._      \\                                       `--\"\"\"\"\"\"\"\"\"\"\"---..._"
-print " __...._\"_-.. \\                       _,                             _..-\"\""
-print " `-.      \"\"\"--`           /       ,-'/|     ,                   _.-\""
-print "    `-.                 , /|     ,'  / |   ,'|    ,|        _..-\""
-print "       `.              /|| |    /   / |  ,'  |  ,' /        ----\"\"\"\"\"\"\"\"\"_`-"
-print "         `.            ( \\  \\      |  | /   | ,'  //                 _.-\""
-print "           `.        .'-\\/'""\\ |  '  | /  .-/'\"`\' //            _.-\""
-print "     /'`.____`-.  ,'\"\\  ''''?-.V`.   |/ .'..-P''''  /\"`.     _.-\""
-print "    '(   `.-._\"\"  ||(?|    /'   >.\\  ' /.<   `\\    |P)||_..-\"___.....---"
-print "      `.   `. \"-._ \\ ('   |     `8      8'     |   `) /\"\"\"\"\"    _\".\"\""
-print "       `.   `.   `.`.b|   `.__            __.'   |d.'  __...--\"\""
-print "          `.   `.   \".`-  .---      ,-.     ---.  -'.-\"\""
-print "            `.   `.   \"\"|      -._      _.-      |\"\""
-print "              `.  .-\"`.  `.       `\"\"\"\"'       ,'"
-print "                `/     `.. \"\"--..__    __..--\"\""
-print "                 `.      /7.--|    \"\"\"\"    |--.__"
-print "                   ..--\"| (  /'            `\\  ` \"\"--.."
-print "                .-\"      \\\\  |\"\"--.    .--\"\"|          \"-."
-print "               <.         \\\\  `.    -.    ,'       ,'     >"
-print "              (P'`.        `%,  `.      ,'        /,' .-\"'?)"
-print "              P    `. \\      `%,  `.  ,'         /' .'     "
-print "             | --\"  _\\||       `%,  `'          /.-'   .    )"
-print "             |       `-.\"\"--..   `%..--\"\"\"\\\\\"--.'       \"-  |"
-print "             \\          `.  .--\"\"\"  \"\\.\\.\\ \\\\.'       )     |"
+print("")
+print("             ___     -._")
+print("             `-. \"\"\"--._ `-.")
+print("                `.      \"-. `.")
+print(" _Seal_           `.       `. \\")
+print(" `-.   \"\"\"---.._    \\        `.")
+print("    `-.         \"-.  \\         `\\")
+print("       `.          `-.\\          \\_.-\"\"\"\"\"\"\"\"--._")
+print("         `.           `                          \"-.")
+print("           `.                                       `.    __....-------...")
+print(" --..._      \\                                       `--\"\"\"\"\"\"\"\"\"\"\"---..._")
+print(" __...._\"_-.. \\                       _,                             _..-\"\"")
+print(" `-.      \"\"\"--`           /       ,-'/|     ,                   _.-\"")
+print("    `-.                 , /|     ,'  / |   ,'|    ,|        _..-\"")
+print("       `.              /|| |    /   / |  ,'  |  ,' /        ----\"\"\"\"\"\"\"\"\"_`-")
+print("         `.            ( \\  \\      |  | /   | ,'  //                 _.-\"")
+print("           `.        .'-\\/'""\\ |  '  | /  .-/'\"`\' //            _.-\"")
+print("     /'`.____`-.  ,'\"\\  ''''?-.V`.   |/ .'..-P''''  /\"`.     _.-\"")
+print("    '(   `.-._\"\"  ||(?|    /'   >.\\  ' /.<   `\\    |P)||_..-\"___.....---")
+print("      `.   `. \"-._ \\ ('   |     `8      8'     |   `) /\"\"\"\"\"    _\".\"\"")
+print("       `.   `.   `.`.b|   `.__            __.'   |d.'  __...--\"\"")
+print("          `.   `.   \".`-  .---      ,-.     ---.  -'.-\"\"")
+print("            `.   `.   \"\"|      -._      _.-      |\"\"")
+print("              `.  .-\"`.  `.       `\"\"\"\"'       ,'")
+print("                `/     `.. \"\"--..__    __..--\"\"")
+print("                 `.      /7.--|    \"\"\"\"    |--.__")
+print("                   ..--\"| (  /'            `\\  ` \"\"--..")
+print("                .-\"      \\\\  |\"\"--.    .--\"\"|          \"-.")
+print("               <.         \\\\  `.    -.    ,'       ,'     >")
+print("              (P'`.        `%,  `.      ,'        /,' .-\"'?)")
+print("              P    `. \\      `%,  `.  ,'         /' .'     ")
+print("             | --\"  _\\||       `%,  `'          /.-'   .    )")
+print("             |       `-.\"\"--..   `%..--\"\"\"\\\\\"--.'       \"-  |")
+print("             \\          `.  .--\"\"\"  \"\\.\\.\\ \\\\.'       )     |")
 
 
-print "\n\nDownloads Complete! Enjoy Reading!"
+print("\n\nDownloads Complete! Enjoy Reading!")
